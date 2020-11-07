d35cf539059ffb77c6b1bfeb594801c3efcc3792
jesse459 <lptvvdm@gmail.com>
2020-11-07 18:01:23 +0300

    Initial commit

---
+++ exercise.py
@@ -0,0 +1,24 @@
+def sentence_maker(sen):
+    interrogatives = ("what", "when", "where", "how", "why")
+    capitalized = sen.capitalize()
+    if sen.startswith(interrogatives):
+        res = "{}?".format(capitalized)
+    else:
+        res = "{}.".format(capitalized)
+    return res
+
+result = []
+n = 1
+
+while True:
+    insentence = str(input("Type your sentence here: "))
+    breaker = "\end"
+
+    if insentence == breaker:
+        break
+    else:
+        result.append(sentence_maker(insentence))
+
+for i in result:
+    print(str(n) + ".", i, "\n")
+    n += 1
